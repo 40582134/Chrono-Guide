1894ce50a5e2e6ceecc12047b65923542b8f7a9f
40582134 <124286253+40582134@users.noreply.github.com>
2024-03-28 04:51:51 +0000

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Chrono-Guide/",
+  base: "/new-Chrono-Guide/",
   build: {
     outDir: "docs",
   },
